251a516abc3774aaccc41f41bf988b5c56c30365
SaraD0304 <saradeluca0403@gmail.com>
2025-03-20 17:04:27 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 export const configurazione = {
-  testo: "G",
+  testo: "C",
 
   dimensione: 0.8,
   interlinea: 0.7,
@@ -7,29 +7,11 @@ export const configurazione = {
   percorsoFont: "./assets/InputMonoCondensed-BoldItalic.ttf",
 
   sensibilitàMicrofonoBase: 1,
-  densitàPuntiBase: 1,
+  densitàPuntiBase: 4,
 
-  nascondiInterfaccia: false,
+  nascondiInterfaccia: true,
 };
 
-/**
- * Disegna punto
- * Metti qui quello che vuoi disegnare per ogni punto della font!
- *
- * @typedef {Object} Ingredienti
- * @property {number} x - La coordinata x del punto
- * @property {number} y - La coordinata y del punto
- * @property {number} angolo - L'angolo della curva della font in quel punto
- * @property {number} indice - Il numero del punto nella sequenza (0, 1, 2, 3, ...)
- * @property {number} unita - Unita' di misura: corrisponde al 10% della dimensione più piccola della finestra (larghezza o altezza)
- * @property {number} volume - Il volume del microfono - Varia da 0 a 1
- * @property {number} frameCount - Il numero di frame passati dall'avvio del programma
- * @property {number} [alpha] - Device orientation alpha angle (z-axis rotation) - Varia da 0 a 360
- * @property {number} [beta] - Device orientation beta angle (front-to-back tilt) - Varia da -90 a 90
- * @property {number} [gamma] - Device orientation gamma angle (left-to-right tilt) - Varia da -90 a 90
- *
- * @param {Ingredienti} ingredienti
- */
 export function disegnaPunto({
   x,
   y,
@@ -42,52 +24,32 @@ export function disegnaPunto({
   beta = 0,
   gamma = 0,
 }) {
-  const size = sin((frameCount + indice) * 6) * ((volume * unita) / 2) * unita;
-
-  if (indice % 2 == 0) {
-    fill("black");
-  } else {
-    fill("white");
-  }
-  noStroke();
-
   push();
-  translate(x, y);
-  ellipse(0, 0, size);
+  let larghezza = map(sin(frameCount * 20), -1, 1, -100, 0);
+  stroke("purple");
+
+  ellipse(x, y, larghezza, 2);
+
   pop();
 }
 
-/**
- * Carica le risorse necessarie
- * Esempio: carica immagini, suoni, ecc.
- */
 export function caricamentoRisorse() {}
 
-/**
- * Imposta le impostazioni iniziali
- * Esempio: impostazioni di frame rate, misura degli angoli, ecc.
- */
+//Imposta le impostazioni iniziali
 export function impostazioni() {
   frameRate(30);
   angleMode(DEGREES);
 }
 
-/**
- * Disegna sotto i punti
- * @param {function} disegnaTesto - La funzione che disegna il testo
- */
+//Disegna sotto i punti
 export function sotto(disegnaTesto) {
-  background("deeppink");
+  background("black");
 
-  // [INFO] Rimuovi il commento per disegnare il testo
-  fill("white");
+  fill("black");
   disegnaTesto();
 }
 
-/**
- * Disegna sopra i punti
- * @param {function} disegnaTesto - La funzione che disegna il testo
- */
+//Disegna sopra i punti
 export function sopra(disegnaTesto) {
   // [INFO] Rimuovi il commento per disegnare il testo
   // fill("black");
